c18654ff7178f12602e327b7b98b1e926e860458
Guillaume Favrot <gui.favrot@gmail.com>
2023-08-09 07:46:00 +0200

    removal of the old MongoDB and of the API folder

--- backend/settings.py
+++ backend/settings.py
@@ -27,9 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api.apps.ApiConfig',
     'corsheaders',
-    'pymongo'
 ]
 
 # Here is the list of all domains that are allowed to make a request to the Django app. 
--- backend/urls.py
+++ backend/urls.py
@@ -10,6 +10,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
     re_path(r'^.*', TemplateView.as_view(template_name='index.html'))
 ]
